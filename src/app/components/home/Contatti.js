9a90935c9361284b0c5ee4607ee7332b64cafe8a
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2025-03-04 22:16:35 +0100

    Aggiunta sezione prodotti

---
+++ src/app/components/home/Contatti.js
@@ -0,0 +1,45 @@
+let Contatti = () => {
+    return (
+        <div className="container">
+            <div className="card p-5 rounded-5 bg-light border-light shadow">
+            <h2 className="text-center">Prodotti</h2>
+            <div className="row row-cols-1 row-cols-md-2 g-4 p-5">
+                <div className="col">
+                    <div className="card rounded-5 bg-light shadow border-light">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card rounded-5 bg-light shadow border-light">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card rounded-5 bg-light shadow border-light">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card rounded-5 bg-light shadow border-light">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contatti;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import ChiSiamo from "./components/home/ChiSiamo";
 import Swiper from "./components/photos/Swiper";
+import Contatti from "./components/home/Contatti";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="container">
       <Swiper/>
-    
       <hr className="featurette-divider"/>
       <ChiSiamo/>
+      <hr className="featurette-divider"/>
+      <Contatti/>
     </div>
   );
 }
